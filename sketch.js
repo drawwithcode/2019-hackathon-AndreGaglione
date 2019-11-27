cbb0cec3c53864b5324e76134b526a759a3a9e34
Andrea Gaglione <a.gaglione7@gmail.com>
2019-11-27 12:51:19 +0100

    second
    final

--- sketch.js
+++ sketch.js
@@ -2,12 +2,14 @@ var bumper;
 var button;
 var texture;
 var volume;
+var font;
 
 //------------------------------------------------PRELOAD
 function preload(){
 
   bumper = loadSound("./assets/TG1_new.mp3");
   earth = loadImage("./assets/earth.jpeg");
+  font = loadFont('assets/Gotham-Black.otf');
 }
 
 //------------------------------------------------SETUP
@@ -22,13 +24,15 @@ function setup() {
   //button
   button = createButton('PLAY/PAUSE');
 
-  tg1 = createGraphics(256,256);
 
   textureMode(NORMAL);
+
+
 }
 
 //------------------------------------------------DRAW
 function draw() {
+
   volume = analyzer.getLevel();
   volume = map(volume,0,1,0,height);
 
@@ -41,7 +45,6 @@ function draw() {
     button.mousePressed(stopMusic);
   }
 
-
   //resize canvas
   windowResized();
 
@@ -51,7 +54,7 @@ function draw() {
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
   button.position(windowWidth/2-button.width/2, windowHeight/2+windowHeight/3);
-//  background('blue');
+
 }
 
 //------------------------------------------------
@@ -72,12 +75,18 @@ function createSphere() {
   //make some interesting sphere move with the mouse
   translate(-mouseX/10, mouseY/10);
   clear();
+  background('black');
   texture(earth);
   //lights
   let dirY = (mouseY / height - 0.5) *2;
   let dirX = (mouseX / width - 0.5) *2;
   directionalLight(250, 250, 250, dirX, -dirY, -2);
   noStroke();
-  sphere(volume, 24, 24);
+  sphere(windowWidth/10+volume, 24, 24);
+  //text
+  fill('white');
+  textFont(font);
+  textSize(50);
+  text('drag to move the earth', 200, 50);
   pop();
 }
